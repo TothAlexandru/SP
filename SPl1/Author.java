6ee9c2706b012eeb494e791f5955d5f1de09b4fe
IcyTroop21 <tth_cristi@yahoo.com>
2018-10-14 13:23:23 +0300

    lab2

---
+++ SPl1/Author.java
@@ -0,0 +1,12 @@
+
+public class Author{
+String nume;
+public Author(String nume) {
+	this.nume = nume;
+}
+public void print() {
+	// TODO Auto-generated method stub
+	System.out.println(nume);
+}
+
+}
---
+++ SPl1/Book.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+String titlu;
+List<Author> a;
+List<Chapter> c;
+public Book(String titlu) {
+	this.a=new ArrayList<>();
+	this.c=new ArrayList<>(); 
+	this.titlu = titlu;
+}
+public void addAuthor(Author a1)
+{
+a.add(a1);
+}
+public int createChapter(String chp) {
+	// TODO Auto-generated method stub
+	Chapter c0=new Chapter(chp);
+	c.add(c0);
+	return c.indexOf(c0);
+}
+public Chapter getChapter(int indexChapter) {
+	// TODO Auto-generated method stub
+	return c.get(indexChapter);
+}
+public void print() {
+	System.out.println(titlu);
+	System.out.println("Authors: ");
+	for(Author auth:a)
+	{
+		auth.print();
+	}
+}
+}
